c1a2de9c34f6803256bd1c8efb75c318177e0bfa
Esbeidy Garcia <esbeidy@Esbeidys-MacBook-Pro.local>
2024-05-09 19:12:06 -0600

    probando

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+# Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-6b61d072-40e4-4f24-ab05-05187d95bf9b.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users/"  # Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH = "/api/v1/kits"  # Almacena la ruta para crear un kit en esta variable
