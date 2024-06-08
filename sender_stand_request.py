da5c84f687894d8c7fbac0f906a5a6075631904f
Esbeidy1 <yasbeck12@gmail.com>
2024-06-07 22:38:32 -0600

    La función 'post_new_user' es para crear usuario y obtener el token de autenticación

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,3 +1,22 @@
 import configuration
 import requests
+import data
 
+
+def post_new_user(body):
+    response = requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                             json=body,
+                             headers=data.headers)
+    return response.json().get("auth_token")
+
+def post_new_client_kit(kit_body, auth_token):
+    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+                         json=kit_body,
+                         headers={
+                             "Content-Type": "application/json",
+                             "Authorization": f"Bearer {auth_token}"
+                         })
+
+response = post_new_client_kit(data.Kit,data.auth_token)
+print(response.status_code)
+print(response.json())
