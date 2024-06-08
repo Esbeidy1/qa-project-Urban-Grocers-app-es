2785f0b17d060b9376ec3f9fc706c71c9a1e65b6
Esbeidy1 <yasbeck12@gmail.com>
2024-06-07 23:26:45 -0600

    Actualizacion de datos para pruebas

--- configuration.py
+++ configuration.py
@@ -1,6 +1,8 @@
 # Inserta tu dirección de URL
-URL_SERVICE = "https://cnt-3e2d5736-985d-440a-a7f7-cbc9f78e74fd.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-2a41d718-24e5-4ebc-bc35-bede9d510a98.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
 
