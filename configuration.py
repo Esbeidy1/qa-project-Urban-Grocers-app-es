bd96730e532f148eb1de8eba1ce72fff91059755
Esbeidy1 <yasbeck12@gmail.com>
2024-06-07 22:56:52 -0600

    Endpoints

--- configuration.py
+++ configuration.py
@@ -1,4 +1,7 @@
-# Inserta tu dirección de URL sin la barra diagonal al final
-URL_SERVICE = "https://cnt-6b61d072-40e4-4f24-ab05-05187d95bf9b.containerhub.tripleten-services.com"
-CREATE_USER_PATH = "/api/v1/users/"  # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = "/api/v1/kits"  # Almacena la ruta para crear un kit en esta variable
+# Inserta tu dirección de URL
+URL_SERVICE = "https://cnt-3e2d5736-985d-440a-a7f7-cbc9f78e74fd.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
+
+
