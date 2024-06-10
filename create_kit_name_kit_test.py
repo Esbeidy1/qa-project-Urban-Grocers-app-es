cfcc53eae0e1767f2847000cf90adcb0be423aa2
Esbeidy1 <yasbeck12@gmail.com>
2024-06-10 13:06:55 -0600

    Actualizado

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -12,29 +12,29 @@ def negative_assert(kit):
     response = sender_stand_request.post_new_client_kit(kit, auth_token)
     assert response.status_code == 400
 
-def test_case_1():
+def test_create_kit_1_letter_in_name_get_success_response():
     positive_assert(data.kit_body_1)
 
-def test_case_2():
+def test_create_kit_with_511_characters_in_name_get_success_response():
     positive_assert(data.kit_body_2)
 
-def test_case_3():
+def test_create_kit_with_0_characters_in_name_get_unsuccessful_response():
     negative_assert(data.kit_body_3)
 
-def test_case_4():
+def test_create_kit_with_512_characters_in_name_get_unsuccessful_response():
     negative_assert(data.kit_body_4)
 
-def test_case_5():
+def test_create_kit_with_special_characters_in_name_get_success_response():
     positive_assert(data.kit_body_5)
 
-def test_case_6():
+def test_create_kit_with_spaces_in_name_get_success_response():
     positive_assert(data.kit_body_6)
 
-def test_case_7():
+def test_create_kit_with_numbers_in_name_get_success_response():
     positive_assert(data.kit_body_7)
 
-def test_case_8():
+def test_create_kit_without_name_parameter_get_unsuccessful_response():
     negative_assert(data.kit_body_8)
 
-def test_case_9():
+def test_create_kit_with_numeric_name_get_unsuccessful_response():
     negative_assert(data.kit_body_9)
